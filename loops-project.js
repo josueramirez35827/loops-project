5680ceba7948ced79b5d9e00c1281a886669715c
Josue Ramirez-Romero <josueramirez35827@gmail.com>
2021-10-06 23:36:13 -0400

    loops-project

---
+++ loops-project.js
@@ -0,0 +1,93 @@
+// Create a function that takes numbers 1 - 10 and console logs them. Use a for loop.
+
+function nums() {
+  for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+}
+nums();
+
+
+
+// Using a while loop, return an array that contains all odd numbers between 3 and 103
+
+let normArray = []
+function normArray() {
+  let oddArray = []
+  let i = 2;
+
+  while (i <= 103) {
+    i++;
+
+    if (i % 2 !== 0) {
+      normArray.push(i);
+      oddArray.push(i);
+    }
+  }
+  console.log(normArray);
+  console.log(oddArray);
+}
+normArray();
+
+
+
+// arraySum(num) receives an array of numbers as an argument and returns the sum of all numbers, 
+// for example: arraySum([1, 2, 3, 4, 5, 6]) => 21, use any loop.
+
+function arraySum(num) {
+  numbers = numbers * num[i];
+
+}
+console.log(numbers);
+console.log(`The total sum of all the numbers is ${numbers}`);
+
+arraySum([3, 18, 6, 65]);
+
+
+
+// Use a loop to display the numbers 9 to 0 in descending order.
+
+function descendingOrder() {
+  for (let i = 9; i >= 0; i--) {
+    console.log(i);
+  }
+}
+console.log(descendingOrder());
+
+
+
+// Write a function computing the factorial of n
+
+function arraySum(num) {
+  n = n * i;
+}
+return `the solution is ${n}`
+
+
+
+// Write a function using a for loop that returns the sum of all numbers from 1 to n.
+
+function arraySum(num) {
+  for (i = 0; i <= n; i++) {
+    sum = sum + i;
+  }
+  console.log(`The sum of n= ${sum}`);
+}
+
+
+
+// Using any loop, Given a string change t=the every second letter to an uppercase ‘Z’. 
+// Assume there are no spaces.
+// Let string = “donuts”
+// Expected output => dZnZtZ
+
+sumNumbers(20);
+function arraySum(num) {
+  return newV;
+}
+
+console.log(changeLetters("Donuts"));
+console.log(changeLetters("Icecream"));
+
+
+
